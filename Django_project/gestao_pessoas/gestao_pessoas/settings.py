6264a175b03304e824f62390c140dc66bf2106d6
Vitor Hugo <vho2009@hotmail.com>
2019-02-13 14:54:18 -0200

    Configuring the app

--- Django_project/gestao_pessoas/gestao_pessoas/settings.py
+++ Django_project/gestao_pessoas/gestao_pessoas/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from decouple import config
+from dj_database_url import parse as dburl
+
+
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,12 +25,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@6#37f90mpr-ouv^c@qt1v%w7_h6#$pf-)68&s@h_=@()_1ua_'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['geastao-clientes.herokuapp.com']
 
 
 # Application definition
@@ -75,16 +80,21 @@ WSGI_APPLICATION = 'gestao_pessoas.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'django',
-        'USER': 'postgres',
-        'PASSWORD': 'django',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-}
+default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+
+DATABASES = { 'default': config('DATABASE_URL', default=default_dburl, cast=dburl), }
+
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'django',
+#         'USER': 'postgres',
+#         'PASSWORD': 'django',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
@@ -127,3 +137,4 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = 'media'
 LOGIN_URL= '/login/'
 LOGIN_REDIRECT_URL= 'nick_list'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
